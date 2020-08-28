7448ca1f3a699d948129fdde68b0036b42cd16b3
emarsico <eam6077@psu.edu>
2020-08-28 03:58:39 +0000

    Fixed for float values

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
-cel = input("Enter temperature in celsius: ") # Stores celsius
-fahr = (9/5)*int(cel) + 32
-print(cel + f"\N{DEGREE SIGN} in Celsius is equivalent to {fahr}\N{DEGREE SIGN} Fahrenheit")
+cel = float(input("Enter temperature in celsius: ")) # Stores celsius
+fahr = (9/5)*float(cel) + 32 # Stores Fahrenheit
+print(str(cel) + "\N{DEGREE SIGN} in Celsius is equivalent to " + str(fahr)+ "\N{DEGREE SIGN} Fahrenheit")

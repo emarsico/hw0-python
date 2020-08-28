6e02534a3bd7c6a28224799a6c6158f1d096405f
emarsico <eam6077@psu.edu>
2020-08-28 03:59:25 +0000

    period.

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 cel = float(input("Enter temperature in celsius: ")) # Stores celsius
 fahr = (9/5)*float(cel) + 32 # Stores Fahrenheit
-print(str(cel) + "\N{DEGREE SIGN} in Celsius is equivalent to " + str(fahr)+ "\N{DEGREE SIGN} Fahrenheit")
+print(str(cel) + "\N{DEGREE SIGN} in Celsius is equivalent to " + str(fahr)+ "\N{DEGREE SIGN} Fahrenheit.")

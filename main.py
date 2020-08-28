ae5670169dec8aa9c1cf616446cdda24bfed6dbd
emarsico <eam6077@psu.edu>
2020-08-28 03:52:25 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,3 @@
+cel = input("Enter temperature in celsius: ") # Stores celsius
+fahr = (9/5)*int(cel) + 32
+print(cel + f"\N{DEGREE SIGN} in Celsius is equivalent to {fahr}\N{DEGREE SIGN} Fahrenheit")
